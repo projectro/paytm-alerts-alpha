77126d2173a3fcbe2eb00339d4de5808fd54cf5e
Dishank Jindal <projectro@Dishanks-MacBook-Pro.local>
2019-06-29 01:51:02 +0530

    fixing import for AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { environment } from 'src/environments/environment.test';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'testing')
   ],
   providers: [],
   bootstrap: [AppComponent]
