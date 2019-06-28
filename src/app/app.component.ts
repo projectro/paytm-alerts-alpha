1c3da66409606304724b6093295affde30c223b6
Dishank Jindal <projectro@Dishanks-MacBook-Pro.local>
2019-06-28 16:27:01 +0530

    work on login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paytm-alerts-alpha';
+  title = environment.name;
+  authSuccess = false;
 }
